cb9d46319571e2fc69b618f2e8686fa326313725
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-08-03 21:45:25 +0200

    Add SecurePasswordHasher.cs, Add LoginScreen.xaml

---
+++ Nedeljni_I_Bojana_Backo/Nedeljni_I_Bojana_Backo/View/LoginScreen.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+
+namespace Nedeljni_I_Bojana_Backo.View
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            var hash = SecurePasswordHasher.Hash("WPFAccess");
+            try
+            {
+                if (txtUsername.Text.Equals("WPFMaster") && SecurePasswordHasher.Verify(txtPassword.Password, hash))
+                {
+                    MasterWindow dashboard = new MasterWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                //else if (JMBGValidation.IsValid(txtUsername.Text) && txtPassword.Password.Equals("Gost"))
+                //{
+                //    Service service = new Service();
+
+                //    if (!service.IsUser(txtUsername.Text))
+                //    {
+                //        Service.userGuest = service.AddUser(txtUsername.Text);
+                //        MainWindow dashboard = new MainWindow();
+                //        dashboard.Show();
+                //        this.Close();
+                //    }
+                //    else
+                //    {
+                //        Service.userGuest = service.FindUser(txtUsername.Text);
+                //        UserWindow userWindow = new UserWindow();
+                //        MainWindow dashboard = new MainWindow();
+                //        userWindow.ShowDialog();
+                //        dashboard.Show();
+                //        this.Close();
+                //    }
+                //}
+                else
+                {
+                    MessageBox.Show("Username or password is incorrect.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //sqlCon.Close();
+            }
+        }
+    }
+}
--- Nedeljni_I_Bojana_Backo/Nedeljni_I_Bojana_Backo/View/MasterWindow.xaml.cs
+++ Nedeljni_I_Bojana_Backo/Nedeljni_I_Bojana_Backo/View/MasterWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Nedeljni_I_Bojana_Backo
+namespace Nedeljni_I_Bojana_Backo.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MasterWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MasterWindow : Window
     {
-        public MainWindow()
+        public MasterWindow()
         {
             InitializeComponent();
         }
